8ce20bfe34e3deac248eec41ea0234d8ecf35dd8
biruk85 <birukm8582@gmail.com>
2023-05-30 15:05:50 -0700

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define class Square"""
+
+class Square:
+    """Represent square"""
+
+    def __init__(self, size):
+        """Initialize new Square.
+
+        Args:
+            size (int): Size of new square
+        """
+        self.__size = size
